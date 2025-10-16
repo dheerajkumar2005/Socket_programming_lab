4420609104304712ef041f8030b1908819b93d38
Dheeraj <23b0920@iitb.ac.in>
2025-10-16 14:23:19 +0530

    modified node.cpp

--- node.cpp
+++ node.cpp
@@ -3,6 +3,7 @@
 // #include <string>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 // #include <netinet/in.h>
 // #include <iostream>
 // #include <unistd.h>
@@ -41,6 +42,8 @@ struct Node {
             cerr << "udp socket creation failed\n";
             exit(1);
         }
+        int flags = fcntl(udp_socket,F_GETFL,0);
+        fcntl(udp_socket,F_SETFL,flags|O_NONBLOCK);
     }
 
     void connect_to_ON() {
@@ -56,7 +59,11 @@ struct Node {
         if(connect(tcp_socket,(struct sockaddr*)&on_addr,sizeof(on_addr))){
             cerr << "connection to Oracle Node failed\n";
             exit(1);
-        };
+        }
+        else{
+            cout << "Connection to ON successfull\n";
+        }
+
         char* message;
         message = new char[6];
         bzero(message,6);
@@ -65,17 +72,41 @@ struct Node {
             cerr << "invalid virtual node ip addr\n";
             exit(1);
         }
+        uint16_t udp_port_be = htons(udp_port);
         memcpy(message,&ip_addr_be,4);
-        memcpy(message+4,&port,2);
-        if(send(tcp_socket,message,4,0) == -1){
+        memcpy(message+4,&udp_port_be,2);
+        ssize_t send_output = send(tcp_socket,message,4,0);
+        if(send_output == -1){
             cerr << "Error in sending the CONNECT message\n";
             exit(1);
         }
+        else if(send_output != 4){
+            cerr << "Supposed to send 4 bytes but " << send_output << " bytes sent\n";
+            exit(1); 
+        }
+        else{
+            cout << "send 4 bytes to ON\n";
+        }
+    }
+
+    void recv_from_ON() {
+        char* link_state;
+        link_state = new char[11];
+        ssize_t recv_output = recv(tcp_socket,link_state,11,0);
+        if(recv_output == -1){
+            cerr << "Error in receiving from ON\n";
+            exit(1);
+        }
+        else if(recv_output != 11){
+            cerr << "upposed to recv 11 bytes but " << recv_output << " bytes received\n";
+        }
+        else{
+            cout << "received 11 bytes (Link-state) from ON\n";
+        }
+        
     }
 
 };
-
-
 
 
 int main(int argc, char* argv[]){
@@ -86,9 +117,9 @@ int main(int argc, char* argv[]){
     const char* vn_ip = argv[1];
     uint16_t vn_udp_port = stoi(argv[2]);
     const char* on_ip = argv[3];
-    uint16_t on_port = 5050;
+    uint16_t on_port = 5000;
 
     Node* vn = new Node(vn_ip,vn_udp_port,on_ip,on_port);
-
+    
 
 }
